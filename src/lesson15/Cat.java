89658112362b87f3148babfb57bc4f46bacdb0ef
AndreyHein <soly.a@web.de>
2024-02-18 16:03:59 +0100

    update lesson15

--- src/lesson15/Cat.java
+++ src/lesson15/Cat.java
@@ -1,31 +1,59 @@
 package lesson15;
 
+import java.util.Scanner;
+
 public class Cat {
+    Scanner scanner = new Scanner(System.in);
+    int food;
+    int[] foodArray = {200, 150, 100, 50};
     String name;
     String color;
     int age;
     int volume;
-    int food;
+    int realVolume;
 
     public Cat(String name, String color, int age, int volume) {
+        realVolume = volume;
         this.name = name;
         this.color = color;
         this.age = age;
         this.volume = volume;
     }
 
-    public Cat() {
-    }
-
-    public void eat(int food) {
-        this.food = food;
+    public void eat() {
+            System.out.println("1. Meat (200 kcal)");
+            System.out.println("2. Fish (150 kcal)");
+            System.out.println("3. Milk (100 kcal)");
+            System.out.println("4. Vegetables (50 kcal)");
+            System.out.print("What to feed a cat? ");
+            int i = scanner.nextInt();
+        this.food = foodArray[i - 1];
+        realVolume += food;
     }
 
     public double satiated() {
-        return  (double) food / volume * 100;
+        return  (double) realVolume / volume * 100;
     }
 
     public void  voice() {
         System.out.println("Meow!");
+    }
+
+    public void ran(){
+        if (realVolume > 50) {
+            realVolume -= 50;
+        } else {
+            System.out.println("The cat can't run, needs to be fed");
+            eat();
+        }
+    }
+
+    public void walk(){
+        if (realVolume > 25) {
+            realVolume -= 25;
+        } else {
+            System.out.println("The cat can't walk, needs to be fed");
+            eat();
+        }
     }
 }
--- src/lesson15/Lesson15.java
+++ src/lesson15/Lesson15.java
@@ -1,5 +1,7 @@
 package lesson15;
 
+import java.util.Scanner;
+
 /**
  * AIT-TR, Java Basic, Lesson #15
  *
@@ -8,14 +10,42 @@ package lesson15;
 
 public class Lesson15 {
     public static void main(String[] args) {
-        Cat cat = new Cat();
-                cat.voice();
-        System.out.println(cat.name + ", " + cat.color + ", " + cat.age);
-
-        Cat cat1 = new Cat("Barsik","black",5, 10);
-        cat1.voice();
-        System.out.println(cat1.name + ", " + cat1.color + ", " + cat1.age);
-        cat1.eat(5);
-        System.out.println(cat1.satiated());
+        Scanner scanner = new Scanner(System.in);
+        Cat cat1 = new Cat("Barsik", "black", 5, 200);
+        System.out.println(cat1.name + ", " + cat1.color + ", " + cat1.age + " years");
+        int action;
+        do {
+            System.out.println("1. Feed the cat");
+            System.out.println("2. The cat is ran");
+            System.out.println("3. The cat is walk");
+            System.out.println("4. The cat is sleep");
+            System.out.print("Select action: ");
+            action = scanner.nextInt();
+            switch (action) {
+                case 1:
+                    cat1.eat();
+                    System.out.println("The cat is " + cat1.satiated() + " % full");
+                    cat1.voice();
+                    break;
+                case 2:
+                    cat1.ran();
+                    System.out.println("The cat lost 50 kcal!");
+                    System.out.println("The cat is " + cat1.satiated() + " % full");
+                    cat1.voice();
+                    break;
+                case 3:
+                    cat1.walk();
+                    System.out.println("The cat lost 25 kcal!");
+                    System.out.println("The cat is " + cat1.satiated() + " % full");
+                    cat1.voice();
+                    break;
+                case 4:
+                    System.out.println("The cat went to bed!");
+                    cat1.voice();
+                    break;
+                default:
+                    System.out.println("Enter is wrong!");
+            }
+        } while (action != 4);
     }
 }
