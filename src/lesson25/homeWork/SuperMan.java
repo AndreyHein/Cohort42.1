ff6cd021b6c3be16bdb1b54bc6dd2a516cfc15c8
AndreyHein <soly.a@web.de>
2024-03-13 18:52:39 +0100

    Update SuperMan.java

--- src/lesson25/homeWork/SuperMan.java
+++ src/lesson25/homeWork/SuperMan.java
@@ -1,6 +1,6 @@
 package lesson25.homeWork;
 
-public class SuperMan extends Human {
+public class SuperMan extends SportMan {
     @Override
     public String run() {
         return "SuperMan run 25 km/h";
