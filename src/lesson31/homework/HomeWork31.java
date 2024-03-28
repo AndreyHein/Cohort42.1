52ad088226f20ca80fa3df44ecde9d811162aa9d
AndreyHein <soly.a@web.de>
2024-03-28 15:22:02 +0100

    update lesson31

--- src/lesson31/homework/HomeWork31.java
+++ src/lesson31/homework/HomeWork31.java
@@ -1,13 +1,7 @@
-package lesson31;
+package lesson31.homework;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.GridLayout;
-import java.awt.Panel;
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
@@ -19,18 +13,24 @@ import java.util.Random;
  *
  * @version 25-Mar-24
  */
-public class HelloSwing extends JFrame {
+public class HomeWork31 extends JFrame {
     private Random random;
     private final Color[] COLORS = {Color.red, Color.green, Color.blue};
     private int CIRCLE_COUNT = 100;
     private int TRIANGLE_COUNT = 10;
     private int RECTANGLE_COUNT = 50;
 
-    public static void main(String[] args) {
-        new HelloSwing();
+    private enum ShapeForm {
+        CIRCLES, RECTANGLES, TRIANGLES;
     }
 
-    HelloSwing() {
+    private ShapeForm shapeForm;
+
+    public static void main(String[] args) {
+        new HomeWork31();
+    }
+
+    HomeWork31() {
         setTitle("Hello, Swing!");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(1000, 700);
@@ -40,14 +40,6 @@ public class HelloSwing extends JFrame {
 
         Canvas canvas = new Canvas();
         canvas.setBackground(Color.WHITE);
-        canvas.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                super.mouseClicked(e);
-                System.out.println(e.getX() + ", " + e.getY());
-                canvas.repaint();
-            }
-        });
 
         DrawTriangle drawTriangle = new DrawTriangle();
         drawTriangle.setBackground(Color.WHITE);
@@ -103,15 +95,42 @@ public class HelloSwing extends JFrame {
         public void paint(Graphics g) {
             super.paint(g);
             for (int i = 0; i < CIRCLE_COUNT; i++) {
-                int d = random.nextInt(20) + 60;
-                int x = random.nextInt(getWidth() - d);
-                int y = random.nextInt(getHeight() - d);
-                Color color = COLORS[random.nextInt(COLORS.length)];
-                g.setColor(color);
-                g.fillOval(x, y, d, d);
-                g.setColor(Color.black);
-                g.drawOval(x, y, d, d);
-
+                if (shapeForm == ShapeForm.CIRCLES) {
+                    int d = random.nextInt(20) + 60;
+                    int x = random.nextInt(getWidth() - d);
+                    int y = random.nextInt(getHeight() - d);
+                    Color color = COLORS[random.nextInt(COLORS.length)];
+                    g.setColor(color);
+                    g.fillOval(x, y, d, d);
+                    g.setColor(Color.black);
+                    g.drawOval(x, y, d, d);
+                }
+                if (shapeForm == ShapeForm.RECTANGLES) {
+                    int width = random.nextInt(20) + 100;
+                    int height = random.nextInt(20) + 100;
+                    int x = random.nextInt(getWidth() - width);
+                    int y = random.nextInt(getHeight() - height);
+                    Color color = COLORS[random.nextInt(COLORS.length)];
+                    g.setColor(color);
+                    g.fillRect(x, y, width, height);
+                    g.setColor(Color.black);
+                    g.drawRect(x, y , width, height);
+                }
+                if (shapeForm == ShapeForm.TRIANGLES) {
+                    int length = random.nextInt(20) + 80;
+                    int h = (int) (length * Math.sqrt(3) / 2);
+                    int x1 = random.nextInt(getWidth() - length);
+                    int y1 = random.nextInt(getHeight() - length);
+                    int x2 = x1 + length;
+                    int y2 = y1;
+                    int x3 = x2 / 2;
+                    int y3 = y2 + h;
+                    Color color = COLORS[random.nextInt(COLORS.length)];
+                    g.setColor(color);
+                    g.drawLine(x1, y1, x2, y2);
+                    g.drawLine(x1, y1, x3, y3);
+                    g.drawLine(x3, y3, x2, y2);
+                }
             }
         }
     }
