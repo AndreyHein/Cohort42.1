c95125d29e3ebf2a607785a031a808b3c999e482
AndreyHein <soly.a@web.de>
2024-03-29 09:42:09 +0100

    refactoring homework31

--- src/lesson31/Lesson31.java
+++ src/lesson31/Lesson31.java
@@ -1,34 +1,42 @@
-package lesson31.homework;
+package lesson31;
 
-import javax.swing.*;
-import java.awt.*;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.GridLayout;
+import java.awt.Panel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.Random;
 
 /**
  * AIT-TR, cohort 42.1, Java Basic, Lesson #31
  *
  * @version 25-Mar-24
  */
-public class HomeWork31 extends JFrame {
-
-    private ShapeForm shapeForm = ShapeForm.CIRCLES;
-    private Shapes shapes = new Shapes();
+public class Lesson31 extends JFrame {
+    private Random random;
+    private final Color[] COLORS = {Color.red, Color.green, Color.blue,Color.yellow,Color.magenta};
+    private int CIRCLE_COUNT = 100;
 
     public static void main(String[] args) {
-        new HomeWork31();
+        new Lesson31();
     }
-
-    HomeWork31() {
+    public Lesson31()  {
         setTitle("Hello, Swing!");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(1000, 700);
+        setSize(500, 500);
         setLocationRelativeTo(null);
 
+        random = new Random();
+
         Canvas canvas = new Canvas();
-        canvas.setBackground(Color.WHITE);
+        canvas.setBackground(Color.white);
         canvas.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
@@ -38,42 +46,21 @@ public class HomeWork31 extends JFrame {
             }
         });
 
-        JButton btnDrawCircle = new JButton("Draw Circle");
-        JButton btnDrawTriangle = new JButton("Draw Triangle");
-        JButton btnDrawRectangle = new JButton("Draw Rectangle");
+        JButton btnDraw = new JButton("Draw");
         JButton btnExit = new JButton("Exit");
 
-        btnDrawCircle.addActionListener(new ActionListener() {
+        btnDraw.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                shapeForm = ShapeForm.CIRCLES;
                 canvas.repaint();
             }
         });
 
         btnExit.addActionListener(e -> System.exit(0));
 
-        btnDrawTriangle.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                shapeForm = ShapeForm.TRIANGLES;
-                canvas.repaint();
-            }
-        });
-
-        btnDrawRectangle.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                shapeForm = ShapeForm.RECTANGLES;
-                canvas.repaint();
-            }
-        });
-
         Panel btnPanel = new Panel();
         btnPanel.setLayout(new GridLayout());
-        btnPanel.add(btnDrawCircle);
-        btnPanel.add(btnDrawTriangle);
-        btnPanel.add(btnDrawRectangle);
+        btnPanel.add(btnDraw);
         btnPanel.add(btnExit);
 
         add(btnPanel, BorderLayout.SOUTH);
@@ -86,7 +73,16 @@ public class HomeWork31 extends JFrame {
         @Override
         public void paint(Graphics g) {
             super.paint(g);
-            shapes.paint(g, shapeForm, this);
+            for (int i = 0; i < CIRCLE_COUNT; i++) {
+                int d = random.nextInt(20) + 60;
+                int x = random.nextInt(getWidth() - d);
+                int y = random.nextInt(getHeight() - d);
+                Color color = COLORS[random.nextInt(COLORS.length)];
+                g.setColor(color);
+                g.fillOval(x, y, d, d);
+                g.setColor(Color.black);
+                g.drawOval(x, y, d, d);
+            }
         }
     }
 }
