190106f6afdbc54e64fc2caaa52e5841dc7a794e
AndreyHein <soly.a@web.de>
2024-04-12 17:56:32 +0200

    lesson36

---
+++ src/lesson36/Lesson36.java
@@ -0,0 +1,26 @@
+package lesson36;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Lesson36 {
+    public static void main(String[] args) {
+        String text = "To be or not to be that is the question " +
+                "Whether it is nobler in the mind to suffer " +
+                "The slings and arrows of outrageous fortune " +
+                "Or to take arms against a sea of troubles " +
+                "And by opposing end them To die to sleep " +
+                "No more and by a sleep to say we end";
+        String[] words = text.toLowerCase().split(" ");
+        Map<String, Integer> wordsMap = new HashMap<>();
+        for (String word : words) {
+            Integer value = wordsMap.get(word);
+            if (value == null) {
+                wordsMap.put(word, 1);
+            } else {
+                wordsMap.put(word, value + 1);
+            }
+        }
+        System.out.println(wordsMap);
+    }
+}
