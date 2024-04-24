07a741c2367c15de52e969916e7c8ebe67186f76
AndreyHein <soly.a@web.de>
2024-04-24 18:39:19 +0200

    Update HomeWork41.java

--- src/lesson41/eployee/HomeWork41.java
+++ src/lesson41/eployee/HomeWork41.java
@@ -3,6 +3,11 @@ package lesson41.eployee;
 import lesson41.eployee.controller.EmployeeUI;
 import lesson41.eployee.repository.EmployeeRepository;
 
+/**
+ * AIT-TR, cohort 42.1, Java Basic, hm # 41
+ * @author Andrey Hein
+ * @version 24-Apr-2024
+ */
 public class HomeWork41 {
     public static void main(String[] args) {
         EmployeeRepository repository = new EmployeeRepository();
