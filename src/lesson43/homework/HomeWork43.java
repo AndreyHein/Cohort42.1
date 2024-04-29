90b5f1671a1d8153f63fd67b07fa7272c762d3e9
AndreyHein <soly.a@web.de>
2024-04-29 17:39:51 +0200

    update homework43

--- src/lesson43/homework/HomeWork43.java
+++ src/lesson43/homework/HomeWork43.java
@@ -2,31 +2,41 @@ package lesson43.homework;
 
 public class HomeWork43 {
     public static void main(String[] args) {
-    // test1: normal
-    // test2: bad size
+        String[][] array = {
+                {"1", "2", "3", "4"},
+                {"5", "6", "7", "8"},
+                {"9", "10", "11", "12"},
+                {"13", "14", "15", "16"}
+        };
 
+        try {
+            int result = calculateArray(array);
+            System.out.println("Sum of array elements: " + result);
+        } catch (MyArraySizeException e) {
+            System.out.println("Array size is not 4x4");
+        } catch (MyArrayDataException e) {
+            System.out.println("Invalid data at: " + e.getMessageDetails());
+        }
     }
-    static int calculateArray(String[][] strArr) {
-        int sumStrArr = 0;
-        if (strArr.length > 4) {
-            throw new MyArraySizeException("strArr.length > 4");
-        }
-        for (String[] rowStrArr : strArr) {
-            if (rowStrArr.length > 4) {
-                throw new MyArraySizeException("strArr.rowStrArr > 4");
-            }
-        }
 
-        for (int row = 0; row < strArr.length; row++) {
-            for (int column = 0; column < strArr[row].length; column++) {
-                String element = strArr[row][column];
+    static int calculateArray(String[][] array) throws MyArraySizeException, MyArrayDataException {
+        int sumArr = 0;
+        if (array.length != 4) {
+            throw new MyArraySizeException("Array size is not 4x4");
+        }
+        for (int row = 0; row < array.length; row++) {
+            if (array[row].length != 4) {
+                throw new MyArraySizeException("Array size is not 4x4");
+            }
+            for (int column = 0; column < array[row].length; column++) {
+                String element = array[row][column];
                 try {
-                    sumStrArr += Integer.parseInt(element);
+                    sumArr += Integer.parseInt(element);
                 } catch (NumberFormatException e) {
-                    throw new MyArrayDataException("row: " + row + ", column: " + column + ", bad element: " + element);
+                    throw new MyArrayDataException("Invalid data",row, column);
                 }
             }
         }
-        return -1;
+        return sumArr;
     }
 }
--- src/lesson43/homework/MyArrayDataException.java
+++ src/lesson43/homework/MyArrayDataException.java
@@ -1,7 +1,14 @@
 package lesson43.homework;
 
 public class MyArrayDataException extends RuntimeException {
-    public MyArrayDataException(String message) {
+    private int row;
+    private int column;
+    public MyArrayDataException(String message, int row, int column) {
         super(message);
+        this.row = row;
+        this.column = column;
+    }
+    public String getMessageDetails() {
+        return getMessage() + "Row: " + row + ", Column: " + column;
     }
 }
