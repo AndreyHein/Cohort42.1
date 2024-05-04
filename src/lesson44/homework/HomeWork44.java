cfafd6a74feae0f0249c2dd7fb83df43435c1e1f
AndreyHein <soly.a@web.de>
2024-05-04 08:39:31 +0200

    update homework44

--- src/lesson44/homework/HomeWork44.java
+++ src/lesson44/homework/HomeWork44.java
@@ -10,29 +10,37 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class HomeWork44 {
     public static void main(String[] args) {
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader("transaktion.txt"));
-            FileWriter lessWriter = new FileWriter("less.txt");
-            FileWriter moreWriter = new FileWriter("more.txt");
+        Map<String, Integer> userMap = new HashMap<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader("transaktion.txt"));
+             FileWriter lessWriter = new FileWriter("less.txt");
+             FileWriter moreWriter = new FileWriter("more.txt")){
 
             String line;
             while ((line = reader.readLine()) != null) {
                 String[] parts = line.split(":");
+                String userName = parts[0];
                 int amount = Integer.parseInt(parts[1]);
-
-                if (amount < 2000) {
-                    lessWriter.write(line + "\n");
-                } else {
-                    moreWriter.write(line + "\n");
+                Integer sum = userMap.get(userName);
+                if (sum == null) {
+                    userMap.put(userName, 0);
+                    sum = 0;
                 }
+                userMap.put(userName,sum + amount);
             }
 
-            reader.close();
-            lessWriter.close();
-            moreWriter.close();
+            for (String userName : userMap.keySet()) {
+                Integer useMoney = userMap.get(userName);
+                if (useMoney < 2000) {
+                    lessWriter.write(userName + ":" + String.valueOf(useMoney) + "\n");
+                } else {
+                    moreWriter.write(userName + ":" + String.valueOf(useMoney) + "\n");
+                }
+            }
 
         } catch (FileNotFoundException e) {
             System.err.println("Input file not found: " + e.getMessage());
